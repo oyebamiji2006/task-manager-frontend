ef933d105be220e24e4d370cabee0d039aad764d
Victor Oyebamiji <victoroyebamiji531@gmail.com>
2026-08-15 15:41:56 +0100

    Save vite.config.js changes

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  root: './',
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: {
-        main: 'index.html'
-      }
-    }
+    emptyOutDir: true
   },
   server: {
     open: true
